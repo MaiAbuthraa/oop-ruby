8d6c5458da7685919855149cce4eb6e7f9fb3257
Mai Abuthraa <maiabuthraa@mais-mbp.mynet>
2020-06-24 09:38:10 +0300

    How To Check If A Block Was Given

--- ruby_yield_keyword.rb
+++ ruby_yield_keyword.rb
@@ -10,7 +10,6 @@ do_somthing do
   puts 'I`m Yield'
 end
 
-
 #Use yield with arguments
 def lets_do_this
   x = rand()
@@ -19,7 +18,10 @@ def lets_do_this
   yield x
   yield x
 end
+########################################
 
+# Every time you call yield, the block will run, so this is like calling the same method again.
+#
 lets_do_this do |argument_block|
   puts argument_block * 100
 end
@@ -30,3 +32,16 @@ end
 # X = 0.7342228868856606
 # 73.42228868856606
 # 73.42228868856606
+########################################
+
+# How To Check If A Block Was Given
+# You can check if a block has been passed in with the block_given? method.
+
+def do_something_with_block
+  return "No block given" unless block_given?
+  yield
+end
+
+puts " How To Check If A Block Was Given?"
+puts do_something_with_block
+do_something_with_block { puts 'I`m Yield' }
